f537af328408528677e4e18c79faea928f725475
chris-koester-db <123766685+chris-koester-db@users.noreply.github.com>
2025-05-06 13:06:31 -0400

    Create file for functions and refactor to return structs

--- databricks_api/get_table.sql
+++ databricks_api/get_table.sql
@@ -1,36 +1,16 @@
 -- Databricks notebook source
--- Function that returns response of "Get a table" API
--- https://docs.databricks.com/api/workspace/tables/get
-create or replace temporary function get_table(table_name string) return
-select
-  parse_json(
-    http_request(
-      conn => 'databricks_api',
-      method => 'GET',
-      path => concat('2.1/unity-catalog/tables/', table_name)
-    ).text
-  ) as resp_variant;
+use catalog identifier(:catalog);
+use schema identifier(:schema);
 
 -- COMMAND ----------
 
--- Call the function
-select get_table('tpcds.sf_10000_liquid.catalog_returns') as table_details
+describe function extended get_table
 
 -- COMMAND ----------
 
--- Parse column details from response
-with resp as (
-select get_table('tpcds.sf_10000_liquid.catalog_returns') as resp_variant
-)
--- Convert array of features to rows
-,response_rows as (
-  select
-    explode(variant_get(resp_variant, '$.columns')::array<variant>) as col
-  from resp
-)
--- Select desired columns
-select
-  col:name::string AS column_name,
-  col:type_name::string AS data_type,
-  col:nullable::boolean AS is_nullable
-from response_rows
+from (select get_table('tpcds.sf_10000_liquid.catalog_returns') as resp_variant)
+|> select explode(variant_get(resp_variant, '$.columns')::array<variant>)
+|> select
+     col:name::string AS column_name,
+     col:type_name::string AS data_type,
+     col:nullable::boolean AS is_nullable
